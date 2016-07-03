ffc6975a68a8a262ecd04ca1121cdcd08b279fa4
Duncan Sample <duncan.sample@comparethemarket.com>
2016-07-03 21:45:31 +0100

    Security: Added HSTS header

--- api/server.js
+++ api/server.js
@@ -9,6 +9,16 @@ const whitelistedOriginDomains = ['example.com', 'example.org']
 // Security: Remove server powered-by header
 app.disable('x-powered-by');
 
+// Security: HSTS
+// Remove if not running on HTTPS
+app.use(helmet.hsts({
+  // Helmet's HSTS middleware takes milliseconds as input
+  maxAge: (31557600 * 1000),
+  includeSubdomains: false,
+  preload: false,
+  force: true
+}))
+
 // Security: CORS
 function checkOriginWhitelist(origin, callback) {
   console.log("origin:", origin)
---
+++ tests/specs/general/hsts.js
@@ -0,0 +1,27 @@
+const chai = require('chai')
+const supertest = require('supertest');
+
+chai.should()
+const expect = chai.expect
+const assert = chai.assert
+
+const api = supertest('http://api:3000')
+
+describe('HSTS', function(){
+  var response
+  var error
+
+  before(function(done){
+    api.get('/')
+      .set('Accept', 'application/json')
+      .end(function(err, res){
+        error = err
+        response = res
+        done()
+      })
+  })
+
+  it('responds with an Strict-Transport-Security header', function() {
+    expect(response.headers['strict-transport-security']).to.equal('max-age=31557600')
+  })
+})
